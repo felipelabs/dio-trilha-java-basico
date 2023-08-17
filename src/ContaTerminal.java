47accb92875b252cf3bd583e552b1e05646175d7
Felipe Neves <felipe.ferreira@saamauditoria.com.br>
2023-08-17 16:56:06 -0300

    submetendo o desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in);
+
+        int numero;
+        String nome;
+        String conta;
+        double saldo;
+
+        System.out.println("Por favor informa o numero da sua agência:");
+        numero = entrada.nextInt();
+
+        System.out.println("Por favor informe seu nome:");
+        nome = entrada.next();
+
+        System.out.println("Por favor informe sua conta:");
+        conta = entrada.next();
+
+        System.out.println("Por favor informe seu saldo:");
+        saldo = entrada.nextDouble();
+
+        System.out.println("Olá ".concat(nome) + ", obrigado por criar uma conta em nosso banco, sua agência "
+        + numero + ", conta ".concat(conta) + " e seu saldo R$" + saldo + " está disponínvel para saque.");
     }
 }
